efaa6aaf4c954788d83cc52d1a93e86e5d2a8c65
KieranOCallaghan-cork <g00412669@atu.ie>
2024-11-04 14:26:28 +0000

    lab6.1

---
+++ lab-2-Lab4.1/BackEnd/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const port = 4000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
+
+const cors = require('cors');
+app.use(cors());
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
